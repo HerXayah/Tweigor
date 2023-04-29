96e5c51f5ae8abe46ccb11e2e0e6bbce3581a297
Sarah Engel <justsweetluna@gmail.com>
2023-04-29 23:21:11 +0200

    This, in practice, can read json and figure out what reg hive it belongs to and what to write with values etc

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Twigor;
+
+internal class Program
+{
+	public static void Main(string[] args)
+	{
+		var json = twigorhelper.ReadJson("test.json");
+
+		Console.WriteLine("Writing to registry...");
+
+		foreach (var line in json)
+		{
+			if (string.IsNullOrEmpty(line.ignore) || bool.TryParse(line.ignore, out bool ignore) && ignore)
+			{
+				Console.WriteLine("Ignored: " + line.key);
+			} else
+			{
+				twigorhelper.WriteToReg(line.hive, line.key, line.name, response =>
+				{
+					Console.WriteLine(response.Contains("Error")
+						? response
+						: "Wrote to registry: " + line.key + " in " + line.hive + " with name " + line.name);
+				});
+			}
+		}
+	}
+
+	private static void HandleError(string errorMessage)
+	{
+		Console.WriteLine(errorMessage);
+	}
+}
+
+public record struct RegTweaks(string hive, string path, string type, string value, string key, string name,
+	string ignore);
---
+++ twigorhelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace Twigor;
+
+public class twigorhelper
+{
+	public delegate void ShootyShootyBangBang(string response);
+
+	// write to registry but check node first where to put it
+	public static void WriteToReg(string hive, string key, string ignore, ShootyShootyBangBang callback)
+	{
+		try
+		{
+			// c# 8 switch expression
+			var node = hive switch
+			{
+				"HKEY_CURRENT_USER" => Registry.CurrentUser,
+				"HKEY_LOCAL_MACHINE" => Registry.LocalMachine,
+				"HKEY_CURRENT_CONFIG" => Registry.CurrentConfig,
+				"HKEY_CLASSES_ROOT" => Registry.ClassesRoot,
+				"HKEY_USERS" => Registry.Users,
+				_ => throw new Exception("Invalid hive")
+			};
+			Console.WriteLine("Writing to registry... in node: " + node);
+
+			// write to registry
+			node.SetValue(key, ignore);
+
+			// call success callback
+			callback("Wrote to registry: " + key + " in " + hive + " with name " + node);
+		} catch (Exception e)
+		{
+			// call error callback
+			callback("Error: " + e.Message);
+		}
+	}
+
+	public static List<RegTweaks>? ReadJson(string fileName)
+	{
+		using (var r = new StreamReader(fileName))
+		{
+			var tw = r.ReadToEnd();
+			return JsonConvert.DeserializeObject<List<RegTweaks>>(tw);
+		}
+	}
+
+	// convert JSON to Indented String
+	public static string JsonToIndentedString(string jsonpath)
+	{
+		if (string.IsNullOrEmpty(jsonpath)) return string.Empty;
+
+		var obj = JsonConvert.DeserializeObject(jsonpath);
+		return JsonConvert.SerializeObject(obj, Formatting.Indented);
+	}
+}
